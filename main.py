431fdbf2d94307d3639ac1adf66b355ce51e0df3
James Luo <jluo@bglcorp.com.au>
2020-07-04 17:40:43 +1000

    init

---
+++ main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+'''Hello to the world from ev3dev.org'''
+
+import os
+import sys
+import time
+
+from ev3dev2.sound import Sound
+from ev3dev2.led import Leds
+
+# state constants
+ON = True
+OFF = False
+
+
+def debug_print(*args, **kwargs):
+    '''Print debug messages to stderr.
+
+    This shows up in the output panel in VS Code.
+    '''
+    print(*args, **kwargs, file=sys.stderr)
+
+
+def reset_console():
+    '''Resets the console to the default state'''
+    print('\x1Bc', end='')
+
+
+def set_cursor(state):
+    '''Turn the cursor on or off'''
+    if state:
+        print('\x1B[?25h', end='')
+    else:
+        print('\x1B[?25l', end='')
+
+
+def set_font(name):
+    '''Sets the console font
+
+    A full list of fonts can be found with `ls /usr/share/consolefonts`
+    '''
+    os.system('setfont ' + name)
+
+
+def main():
+    '''The main function of our program'''
+
+    # set the console just how we want it
+    reset_console()
+    set_cursor(OFF)
+    set_font('Lat15-Terminus24x12')
+
+    # print something to the screen of the device
+    print('Hello World!')
+
+    # print something to the output panel in VS Code
+    debug_print('Hello VS Code!')
+
+    leds = Leds()
+    leds.set_color('LEFT', 'ORANGE')
+    leds.set_color('RIGHT', 'RED')
+
+    sound = Sound()
+    sound.speak('Welcome to the E V 3 dev project!')
+
+    sound.speak('How are you master!')
+
+
+
+    # wait a bit so you have time to look at the display before the program
+    # exits
+    time.sleep(5)
+
+if __name__ == '__main__':
+    main()
